b2b96450d77807ebfc7d8a415963e7395c75afd0
Daniel Koefoed <dc.koefoed@gmail.com>
2021-03-08 08:51:18 +0100

    continued work on routes

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/slack_clone')
+    .then(() => console.log('connected to db'))
+    .catch(error => console.log(error))
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended: false}))
 
 app.use(express.static(path.join(__dirname, 'assets')))
 
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+// REGISTER
+
+router.get('/register', (req, res) => {
+    res.render('register')
+})
+
+router.post('/register', (req, res) => {
+    const {email, username, password} = req.body
+
+    let errors = []
+
+    console.log(`New username: ${username}, email: ${email} and password: ${password}`)
+})
 
 // LOG IN
 
@@ -9,16 +22,6 @@ router.get('/login', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
-
-})
-
-// REGISTER
-
-router.get('/register', (req, res) => {
-    res.render('register')
-})
-
-router.post('/register', (req, res) => {
 
 })
 
